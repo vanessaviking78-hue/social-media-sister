1a6d94ae18aaadabf175b1ef80d115c0817c27f8
Vanessa Wormald <vanessaviking78@gmail.com>
2026-07-27 09:33:13 +0100

    Wire 100 Reels Progress admin page into router

--- artifacts/carousel-generator/src/App.tsx
+++ artifacts/carousel-generator/src/App.tsx
@@ -92,6 +92,7 @@ import CatchUpPlan from "@/pages/catch-up";
 import ResourceLibrary from "@/pages/resources";
 import HealthCheck from "@/pages/health-check";
 import Homework from "@/pages/homework";
+import ReelProgress from "@/pages/reel-progress";
 import BonusContent from "@/pages/bonus-content";
 
 const queryClient = new QueryClient();
@@ -168,6 +169,7 @@ function ProtectedRouter() {
       <Route path="/resources" component={ResourceLibrary} />
 <Route path="/health-check" component={HealthCheck} />
       <Route path="/homework" component={Homework} />
+      <Route path="/reel-progress" component={ReelProgress} />
       <Route path="/bonus-content" component={BonusContent} />
       <Route component={NotFound} />
     </Switch>
