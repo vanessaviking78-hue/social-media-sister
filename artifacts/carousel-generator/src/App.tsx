072587839957cb3019b9149a7f6b53cab4fd5091
Vanessa Wormald <vanessaviking78@gmail.com>
2026-09-24 18:08:32 +0100

    Add route for Magazine component in App.tsx

--- artifacts/carousel-generator/src/App.tsx
+++ artifacts/carousel-generator/src/App.tsx
@@ -1,5 +1,6 @@
 
 import AdventCalendar from "@/pages/advent-calendar";
+import Magazine from "@/pages/magazine";
 import { Switch, Route, Router as WouterRouter, useRoute, useLocation } from "wouter";
 import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -127,6 +128,7 @@ function ProtectedRouter() {
   return (
     <Switch>
       <Route path="/hub" component={Hub} />
+      <Route path="/magazine" component={Magazine} />
       <Route path="/daily-focus" component={DailyFocus} />
       <Route path="/carousel" component={Home} />
       <Route path="/single-image" component={SingleImage} />
