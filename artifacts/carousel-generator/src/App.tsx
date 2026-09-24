ef407299acc03ad0eb0f8d7ee2c8fd9f59b67b46
Vanessa Wormald <vanessaviking78@gmail.com>
2026-09-24 19:32:02 +0100

    Add route for MonthStart component

--- artifacts/carousel-generator/src/App.tsx
+++ artifacts/carousel-generator/src/App.tsx
@@ -2,6 +2,7 @@
 import AdventCalendar from "@/pages/advent-calendar";
 import Magazine from "@/pages/magazine";
 import MagazineMaker from "@/pages/magazine-maker";
+import MonthStart from "@/pages/month-start";
 import { Switch, Route, Router as WouterRouter, useRoute, useLocation } from "wouter";
 import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -132,6 +133,7 @@ function ProtectedRouter() {
       <Route path="/hub" component={Hub} />
       <Route path="/magazine" component={Magazine} />
       <Route path="/magazinemaker" component={MagazineMaker} />
+      <Route path="/monthstart" component={MonthStart} />
       <Route path="/daily-focus" component={DailyFocus} />
       <Route path="/carousel" component={Home} />
       <Route path="/single-image" component={SingleImage} />
